821861b24e1a6ae3dc2c26d9206a97915fea0bce
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-03 21:47:17 +0300

    feat(Layout): add label to ViewController and clean storyboard
    * Added a UILabel to ViewController for display purposes
    * Removed unused components from Main.storyboard to streamline layout

--- 3-Layout/LayoutProject/LayoutProject/ViewController.swift
+++ 3-Layout/LayoutProject/LayoutProject/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let myLabel = UILabel()
+        myLabel.text = "Test Label"
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect.init(x: 10, y: 300, width: 100, height: 100)
+        view.addSubview(myLabel)
     }
 
 

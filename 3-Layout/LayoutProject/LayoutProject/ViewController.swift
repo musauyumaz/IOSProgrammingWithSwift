1f923fb2697c1d8b8e81cffef0ba0e9bbfea8375
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-03 22:04:38 +0300

    feat(LayoutProject): update label positioning in ViewController
    * Adjusted label frame to be responsive based on view dimensions
    * Improved layout for better UI appearance on different screen sizes

--- 3-Layout/LayoutProject/LayoutProject/ViewController.swift
+++ 3-Layout/LayoutProject/LayoutProject/ViewController.swift
@@ -13,10 +13,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
         let myLabel = UILabel()
         myLabel.text = "Test Label"
         myLabel.textAlignment = .center
-        myLabel.frame = CGRect.init(x: 10, y: 300, width: 100, height: 100)
+        myLabel.frame = CGRect.init(x: width * 0.5 - width * 0.8 / 2, y: height * 0.5 - 30, width: width * 0.8, height: 100)
         view.addSubview(myLabel)
     }
 

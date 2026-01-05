1c217ef2dd5578821063974339a0141b8ec6fa30
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-05 20:20:49 +0300

    feat(ViewControllerProject): initialize new iOS project structure
    * Created a new Xcode project with essential files and configurations.
    * Added main storyboard and app delegate for initial setup.

---
+++ 4-BasicIOSFeatures/ViewControllerProject/ViewControllerProject/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  ViewControllerProject
+//
+//  Created by MUSA UYUMAZ on 5.01.2026.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var secondLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ 4-BasicIOSFeatures/ViewControllerProject/ViewControllerProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewControllerProject
+//
+//  Created by MUSA UYUMAZ on 5.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

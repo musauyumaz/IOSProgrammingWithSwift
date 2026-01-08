1851e1d14ad592820436ff9896ef177532001d60
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-08 20:30:07 +0300

    feat(navigation): add detail view controller for landscapes
    * Implemented DetailViewController to display selected landscape details
    * Updated ViewController to manage landscapes and handle navigation

--- 5-TableView/BeautifulPlaces/BeautifulPlaces/ViewController.swift
+++ 5-TableView/BeautifulPlaces/BeautifulPlaces/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var beautifulPlaces: Dictionary<String, String> = [:]
+    var landscapes = [Landscape]()
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,29 +17,46 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
         tableView.delegate = self
         
-        self.beautifulPlaces["Eskişehir - Sazova Parkı"] = "sazova"
-        self.beautifulPlaces["İstanbul - Galata Kulesi"] = "galata"
-        self.beautifulPlaces["İstanbul - Kız Kulesi"] = "kizkulesi"
-        self.beautifulPlaces["Denizli - Pamukkale Travertenleri"] = "pamukkale"
-        self.beautifulPlaces["Trabzon - Uzungöl"] = "uzungol"
-        
+        landscapes.append(Landscape(name: "Sazova Parkı", city: "Eskişehir", image: "sazova"))
+        landscapes.append(Landscape(name: "Galata Kulesi", city: "İstanbul", image: "galata"))
+        landscapes.append(Landscape(name: "Kız Kulesi", city: "İstanbul", image: "kizkulesi"))
+        landscapes.append(Landscape(name: "Pamukkale Travertenleri", city: "Denizli", image: "pamukkale"))
+        landscapes.append(Landscape(name: "Uzungöl", city: "Trabzon", image: "uzungol"))
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.beautifulPlaces.count
+        return self.landscapes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = beautifulPlaces.keys.sorted()[indexPath.row]
+        let landscape = landscapes[indexPath.row]
+        cell.textLabel?.text = "\(landscape.name), \(landscape.city)"
         return cell
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            beautifulPlaces.removeValue(forKey: Array(beautifulPlaces.keys)[indexPath.row])
+            landscapes.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       performSegue(withIdentifier: "toDetailViewController", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailViewController" {
+            let destinationVC = segue.destination as! DetailViewController
+            destinationVC.selectedLandscape = landscapes[tableView.indexPathForSelectedRow!.row]
+        }
+    }
+}
+
+struct Landscape{
+    let name: String
+    let city: String
+    let image: String
 }
 

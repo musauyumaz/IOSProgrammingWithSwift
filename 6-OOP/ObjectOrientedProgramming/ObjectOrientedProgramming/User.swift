a6b010d4399a586befee5696ee738aa04f7c573f
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-08 20:45:52 +0300

    feat(user): add User class and instantiate user objects
    * Introduced User class with properties for name, age, and job
    * Created instances of User and printed their details in main.swift

---
+++ 6-OOP/ObjectOrientedProgramming/ObjectOrientedProgramming/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  ObjectOrientedProgramming
+//
+//  Created by MUSA UYUMAZ on 8.01.2026.
+//
+
+import Foundation
+
+class User{
+    
+    var name = ""
+    var age = 0
+    var job = ""
+}
--- 6-OOP/ObjectOrientedProgramming/ObjectOrientedProgramming/main.swift
+++ 6-OOP/ObjectOrientedProgramming/ObjectOrientedProgramming/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+let user = User()
+user.name = "Musa"
+user.age = 26
+user.job = "Software Developer"
+
+let user2 = User()
+user2.name = "Nazlı Gül"
+user2.age = 23
+user2.job = "Psychologist"
+
+print("\(user.name) is \(user.age) years old and works as a \(user.job).")
+print("\(user2.name) is \(user2.age) years old and works as a \(user2.job).")
+
 

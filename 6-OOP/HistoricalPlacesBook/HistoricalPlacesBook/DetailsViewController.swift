7c794f7d74508ff039e12c7bf9a6184ddca70b53
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-09 23:38:02 +0300

    feat(project): initialize HistoricalPlacesBook project structure
    * Added project files and configurations for HistoricalPlacesBook
    * Included main app components and asset resources

---
+++ 6-OOP/HistoricalPlacesBook/HistoricalPlacesBook/DetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsViewController.swift
+//  HistoricalPlacesBook
+//
+//  Created by MUSA UYUMAZ on 9.01.2026.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lblHistoricalPlace: UILabel!
+    @IBOutlet weak var lblCity: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ 6-OOP/HistoricalPlacesBook/HistoricalPlacesBook/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HistoricalPlacesBook
+//
+//  Created by MUSA UYUMAZ on 9.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+
+}
+

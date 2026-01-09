dd6f3389c44e548bc031a10cba572467d54fec84
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-09 23:51:48 +0300

    feat(ViewController): add historical places data
    * Introduced an array to store historical places.
    * Populated the array with multiple historical place entries.

--- 6-OOP/HistoricalPlacesBook/HistoricalPlacesBook/ViewController.swift
+++ 6-OOP/HistoricalPlacesBook/HistoricalPlacesBook/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    var historicalPlaces : [HistoricalPlace] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        historicalPlaces.append(HistoricalPlace(name: "Taj Mahal", city: "Agra ,Hindistan", image: "tajMahal"))
+        historicalPlaces.append(HistoricalPlace(name: "Water Castle", city: "Montpellier ,France", image: "waterCastle"))
+        historicalPlaces.append(HistoricalPlace(name: "Jama Masjid", city: "Ahmedabad ,India", image: "jamaMasjit"))
+        historicalPlaces.append(HistoricalPlace(name: "Topkapı Sarayı", city: "İstanbul ,Türkiye", image: "topkapi"))
+        historicalPlaces.append(HistoricalPlace(name: "Ayasofya", city: "İstanbul ,Türkiye", image: "ayasofya"))
+        historicalPlaces.append(HistoricalPlace(name: "Sultan Ahmet Camii", city: "İstanbul ,Türkiye", image: "sultanahmet"))
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
